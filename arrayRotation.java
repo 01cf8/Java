995dde238f14c3ab695705aa5e62262e9ff561ec
Akshit Dobriyal <akshitdob14@gmail.com>
2025-08-05 20:00:14 +0530

    Java

--- arrayRotation.java
+++ arrayRotation.java
@@ -21,7 +21,7 @@ public class arrayRotation {
             for(i = 0; i < arr.length-1; i++){
                 arr[i] = arr[i+1];
             }
-            arr[arr.length-1] = first;
+            arr[i] = first;
             p++;
         }
     }
