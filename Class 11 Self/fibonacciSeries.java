074cf276fdb15b7d86cf36c12e5bcc363fdc8944
Akshit Dobriyal <akshitdob14@gmail.com>
2025-08-14 14:23:53 +0530

    Java

--- Class 11 Self/fibonacciSeries.java
+++ Class 11 Self/fibonacciSeries.java
@@ -1,32 +1,18 @@
 public class fibonacciSeries {
-
-    static int[] memo;
-
-    static int fibonacci(int n) {
-        if (memo[n] != -1)
-            return memo[n];
-
-        if (n == 0)
-            memo[n] = 0;
-        else if (n == 1)
-            memo[n] = 1;
-        else
-            memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
-
-        return memo[n];
-    }
-
-    public static void main(String[] args) {
-        int terms = 20;
-        memo = new int[terms + 1];
-
-        for (int i = 0; i <= terms; i++) {
-            memo[i] = -1;
+    public static int fibonacci(int n){
+        if(n>1){
+            return fibonacci(n-1) + fibonacci(n-2);
+        }else if(n==1||n==0){
+            return n;
         }
-
-        System.out.println("Fibonacci series up to " + terms + " terms:");
-        for (int i = 0; i < terms; i++) {
+        return -1;
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        for(int i = 1; i <= n; i++){
             System.out.print(fibonacci(i) + " ");
         }
+        System.out.println();
     }
 }
