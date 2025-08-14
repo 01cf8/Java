623c33e4247c25c6f88cb2e9284df52298823494
Akshit Dobriyal <akshitdob14@gmail.com>
2025-08-14 13:58:41 +0530

    Java

--- Class 11 Self/recursions.java
+++ Class 11 Self/recursions.java
@@ -1,14 +1,14 @@
 public class recursions {
     public static int factorial(int n){
-        if(n >= 1){
-            return (n * factorial(n-1));
+        if(n>1){
+            return n*factorial(n-1);
         }
         else{
             return 1;
         }
     }
     public static void main(String[] args) {
-        long f = factorial(5);
-        System.out.println("The factorial is: "+f);
+        factorial(5);
+        System.out.println("Factorial of 5 is: " + factorial(5));
     }
 }
